62f3b6114aa989b1226896f8a7bd82e08d2bfbb8
SolarSpectre <josephcaza32@gmail.com>
2025-05-27 16:13:16 -0500

    readme

--- src/app/supabase.client.ts
+++ src/app/supabase.client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase_url = 'https://emdygoofltkbjibzftai.supabase.co'
-const anon_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtZHlnb29mbHRrYmppYnpmdGFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyOTcxNzcsImV4cCI6MjA2Mzg3MzE3N30.5XOaFp3E4HWTzQtYebxMxRPvUq8O6NordYYVPJ42yGM'
+const supabase_url = ''
+const anon_key = ''
 
 export const supabase = createClient(supabase_url, anon_key)
